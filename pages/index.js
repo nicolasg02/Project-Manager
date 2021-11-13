80590f9c56e43f317764c252185ed448217697ed
Nicolas Salieri <nicolasg02@M1MacBookAir.local>
2021-11-12 23:22:41 -0300

    Changelog

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,7 @@ export default function index() {
         <Navbar />
         {/* <Home /> */}
         {/* <Overview /> */}
-        {/* <Changelog /> */}
-        STACK
+        <Changelog />
       </div>
     </div>
   );
