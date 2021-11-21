c23b26feb3d929f0e810208cd3743596e6506edc
Nicolas Salieri <nicolasg02@M1MacBookAir.local>
2021-11-20 21:18:34 -0300

    nextjs env config

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,14 @@
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG);
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: 'project-manager-f6dc7.firebaseapp.com',
+  projectId: 'project-manager-f6dc7',
+  storageBucket: 'project-manager-f6dc7.appspot.com',
+  messagingSenderId: '445619031026',
+  appId: '1:445619031026:web:56386b441fc077502db96b',
+};
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
