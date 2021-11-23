02c21174a074871877f2261d8d60cf6c8ed330b8
Nicolas Salieri <nicolasg02@M1MacBookAir.local>
2021-11-23 15:51:23 -0300

    layout bugfix

--- pages/project/[id]/changelog/index.js
+++ pages/project/[id]/changelog/index.js
@@ -1,34 +1,37 @@
+import Layout from '../../../../components/Layout';
 import Accordion from '../../../../components/changelog/Accordion';
 
 function Changelog() {
   return (
-    <div className="max-h-screen overflow-y-scroll flex-1">
-      <div className="max-w-4xl mx-auto my-10 px-2 xl:px-0 p-10 flex flex-col space-y-10">
-        <h1 className="text-4xl">Changelog</h1>
-        <hr className="border-gray-700" />
-        <div className="flex flex-col space-y-2 bg-gray-100 p-5 rounded jusitfy-around">
-          <h3 className="text-xl">Register</h3>
-          <input
-            className="border rounded p-2"
-            placeholder="Enter Version number.."
-          />
-          <textarea
-            className="border rounded p-4"
-            placeholder="Changelogs here.."
-            rows="10"
-          />
-          <button className="bg-green-500 px-6 py-2 rounded text-md hover:bg-green-400">
-            Create
-          </button>
-        </div>
-        <hr className="border-gray-700" />
-        <div className="space-y-8">
-          <Accordion />
-          <Accordion />
-          <Accordion />
+    <Layout>
+      <div className="max-h-screen overflow-y-scroll flex-1">
+        <div className="max-w-4xl mx-auto my-10 px-2 xl:px-0 p-10 flex flex-col space-y-10">
+          <h1 className="text-4xl">Changelog</h1>
+          <hr className="border-gray-700" />
+          <div className="flex flex-col space-y-2 bg-gray-100 p-5 rounded jusitfy-around">
+            <h3 className="text-xl">Register</h3>
+            <input
+              className="border rounded p-2"
+              placeholder="Enter Version number.."
+            />
+            <textarea
+              className="border rounded p-4"
+              placeholder="Changelogs here.."
+              rows="10"
+            />
+            <button className="bg-green-500 px-6 py-2 rounded text-md hover:bg-green-400">
+              Create
+            </button>
+          </div>
+          <hr className="border-gray-700" />
+          <div className="space-y-8">
+            <Accordion />
+            <Accordion />
+            <Accordion />
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
--- pages/project/[id]/overview/index.js
+++ pages/project/[id]/overview/index.js
@@ -2,56 +2,60 @@ import { useRouter } from 'next/router';
 
 import { FaGithub, FaGlobe } from 'react-icons/fa';
 
+import Layout from '../../../../components/Layout';
+
 function Overview() {
   const router = useRouter();
   const { id } = router.query;
 
   return (
-    <div className="max-h-screen overflow-y-scroll flex-1">
-      <div className="max-w-4xl mx-auto py-10 px-2 xl:px-0">
-        {/* Project Name */}
-        <div>
-          <h1 className="text-center text-3xl sm:text-4xl py-8 bg-gray-100 text-gray-800 rounded font-bold">
-            To Do App
-          </h1>
-        </div>
-        {/* Repo */}
-        <div className="my-3 border border-gray-700 flex rounded bg-white cursor-default">
-          <div className="bg-gray-700 text-gray-50 flex items-center space-x-2 p-2 max-w-3xl">
-            <FaGithub className="h-4 w-4" />
-            <p className="text-xs sm:text-sm">Github Repository</p>
+    <Layout>
+      <div className="max-h-screen overflow-y-scroll flex-1">
+        <div className="max-w-4xl mx-auto py-10 px-2 xl:px-0">
+          {/* Project Name */}
+          <div>
+            <h1 className="text-center text-3xl sm:text-4xl py-8 bg-gray-100 text-gray-800 rounded font-bold">
+              To Do App
+            </h1>
           </div>
-          <div className="p-2">
-            <a className="text-blue-500 hover:text-blue-600 cursor-pointer text-xs sm:text-sm overflow-x-hidden flex items-center">
-              https://github.com/nicolasg02/Project-Manager
-            </a>
+          {/* Repo */}
+          <div className="my-3 border border-gray-700 flex rounded bg-white cursor-default">
+            <div className="bg-gray-700 text-gray-50 flex items-center space-x-2 p-2 max-w-3xl">
+              <FaGithub className="h-4 w-4" />
+              <p className="text-xs sm:text-sm">Github Repository</p>
+            </div>
+            <div className="p-2">
+              <a className="text-blue-500 hover:text-blue-600 cursor-pointer text-xs sm:text-sm overflow-x-hidden flex items-center">
+                https://github.com/nicolasg02/Project-Manager
+              </a>
+            </div>
           </div>
-        </div>
-        {/* Live */}
-        <div className="my-3 border border-gray-700 flex rounded bg-white cursor-default">
-          <div className="bg-gray-700 text-gray-50 flex items-center space-x-2 p-2 w-3xl">
-            <FaGlobe className="h-4 w-4" />
-            <p className="text-xs sm:text-sm">Live Preview</p>
+          {/* Live */}
+          <div className="my-3 border border-gray-700 flex rounded bg-white cursor-default">
+            <div className="bg-gray-700 text-gray-50 flex items-center space-x-2 p-2 w-3xl">
+              <FaGlobe className="h-4 w-4" />
+              <p className="text-xs sm:text-sm">Live Preview</p>
+            </div>
+            <div className="p-2">
+              <a className="text-blue-500 hover:text-blue-600 cursor-pointer text-xs sm:text-sm overflow-x-hidden flex items-center">
+                https://github.com/nicolasg02/Project-Manager
+              </a>
+            </div>
           </div>
-          <div className="p-2">
-            <a className="text-blue-500 hover:text-blue-600 cursor-pointer text-xs sm:text-sm overflow-x-hidden flex items-center">
-              https://github.com/nicolasg02/Project-Manager
-            </a>
-          </div>
-        </div>
 
-        <hr className="border-gray-400 my-16" />
+          <hr className="border-gray-400 my-16" />
 
-        {/* README */}
-        <div className="flex justify-between px-2">
-          <h2 className="text-3xl font-thin">README</h2>
-          <button>Edit</button>
-        </div>
-        <div className="bg-gray-100 rounded p-10 h-screen">
-          MARKOWN EDITOR HERE
+          {/* README */}
+          <div className="flex justify-between px-2">
+            <h2 className="text-3xl font-thin">README</h2>
+            <button>Edit</button>
+          </div>
+          <div className="bg-gray-100 rounded p-10 h-screen">
+            MARKOWN EDITOR HERE
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
