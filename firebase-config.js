c5c6ba30205e0944d058c76cabe15c1889c5b40e
Nicolas Garcia <68668870+nicolasg02@users.noreply.github.com>
2022-01-04 01:45:11 -0300

    New firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,15 @@
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: 'project-manager-f6dc7.firebaseapp.com',
-  projectId: 'project-manager-f6dc7',
-  storageBucket: 'project-manager-f6dc7.appspot.com',
-  messagingSenderId: '445619031026',
-  appId: '1:445619031026:web:56386b441fc077502db96b',
+  authDomain: 'project-manager-60cfe.firebaseapp.com',
+  projectId: 'project-manager-60cfe',
+  storageBucket: 'project-manager-60cfe.appspot.com',
+  messagingSenderId: '10760229753',
+  appId: '1:10760229753:web:5df49c191a928b5d8955a8',
 };
 
 // Initialize Firebase
